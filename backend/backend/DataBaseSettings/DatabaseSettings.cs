9902b36dce385cb55d07bbf79b275a74c2ce8998
Tanay Kumar <tanay.kumar@capsitech.com>
2026-09-08 15:04:02 +0530

    Updated database settings

---
+++ backend/backend/DataBaseSettings/DatabaseSettings.cs
@@ -0,0 +1,8 @@
+﻿namespace backend.DataBaseSettings
+{
+    public class DatabaseSettings
+    {
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+}
--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -1,3 +1,5 @@
+using backend.DataBaseSettings;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.Configure<DatabaseSettings>(builder.Configuration.GetSection("DatabaseSettings"));
 
 var app = builder.Build();
 
