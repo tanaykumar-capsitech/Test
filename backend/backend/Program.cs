33d3f4857919e52b53bd43e72ae13ba6829f88a5
Tanay Kumar <tanay.kumar@capsitech.com>
2026-09-09 18:35:49 +0530

    Added swagger

--- backend/backend/Program.cs
+++ backend/backend/Program.cs
@@ -53,11 +53,10 @@ builder.Services.AddScoped<AuthService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
+
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 app.UseHttpsRedirection();
 app.UseCors("AllowOrigin");
