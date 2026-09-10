f64b61d4a683a4efefc80a6045b8bb3aa2cfccf3
Tanay Kumar <tanay.kumar@capsitech.com>
2026-09-10 18:57:01 +0530

    fixed bugs

--- frontend/src/Pages/Register.tsx
+++ frontend/src/Pages/Register.tsx
@@ -22,7 +22,7 @@ const Register = () => {
         const response = await axios.post(url + 'Auth/register', values)
         setLoading(false)
 
-        if (response.data.statusCode != 201) {
+        if (response.data.statusCode != 200) {
             setError(response.data.message)
 
             return
